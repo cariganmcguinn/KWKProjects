45482f3a0f9db0b51573b80a458046a1a4626b65
cariganmcguinn <68654801+cariganmcguinn@users.noreply.github.com>
2020-07-22 11:52:27 -0400

    Add files via upload

---
+++ KWK XCode Files/KWK Project 1/KWK Project 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KWK Project 1
+//
+//  Created by Carigan Mcguinn on 7/22/20.
+//  Copyright © 2020 Carigan Mcguinn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
