4b099f937f201a192c804531e7cf7df0b25fd746
cariganmcguinn <68654801+cariganmcguinn@users.noreply.github.com>
2020-07-27 14:40:06 -0400

    ToDoList Iteration 0

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,12 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Carigan Mcguinn on 7/27/20.
+//  Copyright © 2020 Carigan Mcguinn. All rights reserved.
+//
+class ToDoClass {
+    var description = ""
+    var important = false
+}
+
